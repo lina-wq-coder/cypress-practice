a73994f75601c97b1fb86a32002525ca0c3f3bf6
Galina Z <g.n.zamyatina@gmail.com>
2025-11-18 19:58:32 +0300

    Update staya.js

--- cypress/integration/1-My-tests/staya.js
+++ cypress/integration/1-My-tests/staya.js
@@ -1,14 +1,17 @@
-
-describe('Тестирование staya', function () {
-    
-    it('Проваливаюсь в категорию поводки и ищу совпадение с названием Heatwave', function () {
+describe('Тест добавления в корзину', function () {
+    it('[+]Ищу товар, добавляю в корзину, удаляю', function () {
         cy.visit('https://staya.dog/');
-        cy.contains('input', 'Поводки').click();
-        cy.contains('Heatwave');
-        cy.end();
-         })
+        cy.get('#__layout > div > main > div > section.home-categories.IndexPage__categories > a:nth-child(2) > div > canvas').click();
+        cy.contains('Urban Violet').click();
+        cy.xpath('//*[@id="ProductPage::CardForm"]/div[3]/div[1]/div[2]/div/div/label[1]').click();
+        cy.xpath('//*[@id="ProductPage::CardForm"]/div[3]/div[2]/div[2]/div/div/label[1]').click();
+        cy.xpath('//*[@id="__layout"]/div/main/div/div/div[3]/div/div/button').click();
+        cy.xpath('//*[@id="__layout"]/div/section/section/footer/div[2]/a').click();
+        cy.contains('Заполните данные получателя');
+        cy.xpath('//*[@id="__layout"]/div/main/div/div[2]/div/div[2]/div/section/div[1]/div').click();
+    })
 
-     it('search something', function () {
-        cy.visit('https://staya.dog/');
-        })
-})
+    it('[+]Ищу товар, добавляю в корзину, удаляю', function () {
+       cy.visit('https://staya.dog/');
+    })
+ })
